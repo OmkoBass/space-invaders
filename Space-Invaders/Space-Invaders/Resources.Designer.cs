71470f97a0fe3fa30123c923c939459a9d654c27
OmkoBass <omkob@hotmail.com>
2022-04-11 11:39:18 +0200

    bin debug

--- Space-Invaders/Space-Invaders/Resources.Designer.cs
+++ Space-Invaders/Space-Invaders/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Space_Invaders {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap enemyBomb {
+            get {
+                object obj = ResourceManager.GetObject("enemyBomb", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap player {
             get {
                 object obj = ResourceManager.GetObject("player", resourceCulture);
