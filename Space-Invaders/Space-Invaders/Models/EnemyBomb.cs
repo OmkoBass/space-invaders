7e7024fd26b5907d9e62a39cf3e34cb87e83b487
OmkoBass <omkob@hotmail.com>
2022-04-12 17:04:28 +0200

    add enemies that inherit EnemyBase class

---
+++ Space-Invaders/Space-Invaders/Models/EnemyBomb.cs
@@ -0,0 +1,24 @@
+﻿namespace Space_Invaders.Models
+{
+    internal class EnemyBomb : EnemyBase
+    {
+        internal List<EnemyBase> Neighbours { get; set; }
+        public EnemyBomb(Point Position, List<EnemyBase> neighbours) : base(Position, null)
+        {
+            Neighbours = neighbours;
+        }
+
+        internal override void Die()
+        {
+            this.IsDead = true;
+
+            foreach (var enemy in this.Neighbours)
+            {
+                if(enemy != null && !enemy.IsDead)
+                {
+                    enemy.Die();
+                }
+            }
+        }
+    }
+}
---
+++ Space-Invaders/Space-Invaders/Models/EnemyRegular.cs
@@ -0,0 +1,12 @@
+﻿namespace Space_Invaders.Models
+{
+    internal class EnemyRegular : EnemyBase
+    {
+        public EnemyRegular(Point Position, Bitmap Sprite) : base(Position, Sprite) { }
+
+        internal override void Die()
+        {
+            this.IsDead = true;
+        }
+    }
+}
