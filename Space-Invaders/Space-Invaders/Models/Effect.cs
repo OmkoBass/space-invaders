e4eec066df04697bf573e4dd4ba44e01f81ac47b
OmkoBass <omkob@hotmail.com>
2022-04-22 23:53:04 +0200

    add effect class to show some random spinning rectangles

---
+++ Space-Invaders/Space-Invaders/Models/Effect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders.Models
+{
+    internal class Effect
+    {
+        internal Point Position { get; set; }
+        internal Size Size = new Size(12, 12);
+
+        private GraphicsPath groupPath = new GraphicsPath();
+        private Matrix rotateMatrix = new Matrix();
+
+        internal Effect(Point Position)
+        {
+            this.Position = Position;
+
+            groupPath.AddRectangle(new Rectangle(this.Position, this.Size));
+            rotateMatrix.RotateAt(12, this.Position);
+        }
+
+        internal void Draw(Graphics g, Pen pen)
+        {
+            groupPath.Transform(rotateMatrix);
+            g.DrawPath(pen, this.groupPath);
+        }
+    }
+}
